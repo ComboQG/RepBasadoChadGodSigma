fe29b7cbda85b6b157d4bea24d585322656cacb5
AnthonySigma <agustin.thebrain@gmail.com>
2024-09-03 21:54:01 -0600

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -15,3 +15,9 @@ public class HolaMundo {
         System.out.println("Hola, mundo!"); //Hecho por Andres Gustavo Rivera V.
     }
 }
+
+public class HolaReal {
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");//Hecho por el ING ANTHONY AGUSTIN
+}
+
