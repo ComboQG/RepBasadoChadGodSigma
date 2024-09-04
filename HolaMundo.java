02c7d44d720f4072b3032b9c44cd2f209c48599d
gremorytec <L22020806@veracruz.tecnm.mx>
2024-09-03 21:20:14 -0600

    Update HolaMundo.java
    yo raul eduardo romero ramirez agregue mi hola mundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,3 +3,9 @@ public class HolaMundo {
         System.out.println("Hola, Mundo!"); //Hecho por Manuel Díaz Acuña
     }
 }
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola, mundo!"); //Hecho por Raul Eduardo Romero Ramirez
+    }
+}
