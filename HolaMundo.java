c848e710530194be8d1bc298777c4a5302a72ece
AndyGuss <andres.rivera784@gmail.com>
2024-09-03 21:31:26 -0600

    Update HolaMundo.java
    Se le agrego otro "Hola Mundo"

--- HolaMundo.java
+++ HolaMundo.java
@@ -9,3 +9,9 @@ public class HolaMundo {
         System.out.println("Hola, mundo!"); //Hecho por Raul Eduardo Romero Ramirez
     }
 }
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola, mundo!"); //Hecho por Andres Gustavo Rivera V.
+    }
+}
